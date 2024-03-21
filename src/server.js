4e2de3cd3cc6714072d8563d44ebb2f31e97fa97
Gbbbbbbbbb <gbbb9767@gmail.com>
2024-03-21 15:56:56 +0900

    study 3.4

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const Port = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send(" I still love you. ");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on port 4000 http://localhost:${Port}`);
 

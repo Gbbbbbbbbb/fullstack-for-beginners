e2bc58d8fad7aa6702d83acd4d2141984a1aa622
Gbbbbbbbbb <gbbb9767@gmail.com>
2024-03-22 21:43:38 +0900

    :id에 따른 변수의 변화 req.params로 확인

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,4 +3,7 @@ export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("remove User");
 export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Logout");
-export const see = (req, res) => res.send("See User");
+export const see = (req, res) => {
+  console.log(req.params);
+  res.send("See User");
+};

d35481d7e116257b1c0e89c42e5281e1dfd770f9
Gbbbbbbbbb <gbbb9767@gmail.com>
2024-03-22 22:17:45 +0900

    npm i pug 5.1

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,10 +4,11 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
 
 //request는 제일 위에 것 부터봄
+//(\\d+) 정규식

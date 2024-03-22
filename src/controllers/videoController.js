a27270fd76351b7ad65f191c6211a4301e3ac060
Gbbbbbbbbb <gbbb9767@gmail.com>
2024-03-22 23:52:33 +0900

    stduy 5.4

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => {
-  res.render("home");
+  res.render("home", { pageTitle: "Home" });
 };
 
 export const see = (req, res) => {
@@ -11,3 +11,5 @@ export const edit = (req, res) => {
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
+
+//res.send 에 우리는 html을 보낼수있었다 하지만 그렇게 하면 코드가 복잡해지므로 pug라는 view 템플릿을 사용하여 렌더링을통해서 html을 형성할 수 있었다 랜더링에느 view의 이름이 들어간다 view의 이름은  .pug 로 끝난다

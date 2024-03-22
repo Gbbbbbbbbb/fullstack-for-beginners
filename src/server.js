d7dfd82f932c9db962cfe0b56c03215c5e61949f
Gbbbbbbbbb <gbbb9767@gmail.com>
2024-03-22 22:31:05 +0900

    study 5.1 currend work directory

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import videoRouter from "./routers/videoRouter";
 
 const Port = 4000;
 
+console.log(process.cwd()); // 현재 작업 디렉토리 찾는 코드 current work directory package.json에서 서버를 시작하면 이것이 현재 작업 디렉토리가 된다 현재 작업 디렉토리는 노드를 시작하는 디렉토리라는 것만 기억하자
 const app = express();
 const logger = morgan("dev"); //combined 도있다
 app.use(logger);

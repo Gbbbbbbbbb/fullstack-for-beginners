4404273fd79f73ba2cd46f15e499569026271c53
Gbbbbbbbbb <gbbb9767@gmail.com>
2024-06-12 20:49:14 +0900

    study 6.19

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -10,10 +10,10 @@ import {
 
 const videoRouter = express.Router();
 
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-videoRouter.get("/upload", getUpload);
-videoRouter.post("/upload", postUpload);
+
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);

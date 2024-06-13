608446f1eb4f64a6fc839b80bcb02b5d26bcdd66
Gbbbbbbbbb <gbbb9767@gmail.com>
2024-06-13 17:24:06 +0900

    study 6.27

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,7 +6,7 @@ const handleSearch = (error, videos) => {
 };
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 //fakeUser:fakeUser = fakeUser
@@ -46,7 +46,6 @@ export const postEdit = async (req, res) => {
   //   .map((word) => (word.startsWith("#") ? word : `#${word}`)); 위에랑 같은 것
   return res.redirect(`/videos/${id}`);
 };
-export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 
 export const getUpload = (req, res) => {
@@ -76,4 +75,16 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
+};
 //res.send 에 우리는 html을 보낼수있었다 하지만 그렇게 하면 코드가 복잡해지므로 pug라는 view 템플릿을 사용하여 렌더링을통해서 html을 형성할 수 있었다 랜더링에sms view의 이름이 들어간다 view의 이름은  .pug 로 끝난다
